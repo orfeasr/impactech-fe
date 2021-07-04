69fe4059b128730ba46f72311579a2c2c5c1fc25
orfeasr <orfeasced@gmail.com>
2021-07-04 15:04:11 +0300

    feat(dashboard) Display the last 10 received messages

--- projects/impact-notifications/src/app/dashboard/dashboard.component.ts
+++ projects/impact-notifications/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationsService } from '../../services/notifications.service';
+import { AuthService } from '../../../../impact-common/src/lib/authentication/auth.service';
+
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  recentMessages;
 
-  constructor() { }
+  constructor(
+    private notifications: NotificationsService,
+    private auth: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    const user = this.auth.userDetails.username;
+    this.notifications.getMessages(user).subscribe(
+      res => {
+        const totalNumOfMessages = res.length;
+        this.recentMessages = res.slice(totalNumOfMessages - 10, totalNumOfMessages);
+      }
+    );
   }
 
 }
